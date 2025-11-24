246208b488d464249af9d96b4adc4a0e7d48e2e0
asierhv <asierherranzv@gmail.com>
2025-11-24 12:19:59 +0100

    Minimal changes and README

--- run_subprocess_spp.sh
+++ run_subprocess_spp.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Wrapper taht activates a minimal venv for executing the speech_processing_pipeline from another script using subprocess
+# Wrapper that activates a minimal venv for executing the speech_processing_pipeline from another script using subprocess
 # The .py script prints a json so it can be captured later.
 
-source .venvs/spp_venv/bin/activate
+source .venvs/ahosphitz/bin/activate # Change this path to you point to your enviroment
+
 python speech_processing_pipeline.py \
-    --run_mode="$1" \
-    --audio_file="$2" \
-    --input_text="$3" \
-    --out_path="$4" \
-    --seg_model="$5" \
-    --seg_config_yml="$6" \
-    --seg_option="$7" \
-    --stt_model="$8" \
-    --cp_model="$9" \
-    --device="$10"
+    --run_mode="${1}" \
+    --audio_file="${2}" \
+    --input_text="${3}" \
+    --out_path="${4}" \
+    --seg_model="${5}" \
+    --seg_config_yml="${6}" \
+    --seg_option="${7}" \
+    --stt_model="${8}" \
+    --cp_model="${9}" \
+    --device="${10}"
